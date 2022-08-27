652a4ca5f4db0717d8926b4c6888b43c51adee05
SaberGuirat <s.guirat@digital-expert.io>
2022-08-27 22:14:21 +0200

    feat: image filtering

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -14,7 +14,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
       const photo = await Jimp.read(inputURL);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
+      photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
@@ -32,8 +32,12 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files: Array<string>) {
-  for (let file of files) {
-    fs.unlinkSync(file);
+export function deleteLocalFiles(files: Array<string>) {
+  try {
+    for (let file of files) {
+      fs.unlinkSync(file);
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
